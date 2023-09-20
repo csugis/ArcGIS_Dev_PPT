043b34a353a844ae1b240d1c149d1d8b3ad1a682
BaoyiZhang-CSU <zhangbaoyi@csu.edu.cn>
2023-09-20 10:33:54 +0800

    Chapter 3

---
+++ Chapter 3/ArcMapAddinDemo/ArcMapAddinDemo/ButtonListFeature.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace ArcMapAddinDemo
+{
+    public class ButtonListFeature : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonListFeature()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+            IMap pMap = ArcMap.Document.FocusMap;
+            List<IFeature> list = new List<IFeature>();
+            IEnumFeature pEnumFeat = (IEnumFeature)pMap.FeatureSelection;
+            pEnumFeat.Reset();
+            IFeature pfeat = pEnumFeat.Next();
+            while (pfeat != null)
+            {
+                list.Add(pfeat);
+                pfeat = pEnumFeat.Next();
+            }
+            MessageBox.Show("Selected Features:" + list.Count.ToString(), "CSUGIS");
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
